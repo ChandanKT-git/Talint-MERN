44bb49e9dbe086013cd5b23efe9caa94cabff74c
ChandanKT-git <chandukt29092004@gmail.com>
2026-01-21 20:38:41 +0530

    fix: update CORS and CLIENT_URL for production"

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,12 @@ const __dirname = path.dirname(__filename);
 // middleware
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: ENV.NODE_ENV === "production" ? process.env.CLIENT_URL : "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
